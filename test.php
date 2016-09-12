fcbc22c10012071554fa4faaedcca4265e1d3bd2
igor.bubnevich <igor.bubnevich@zed.com>
2016-09-12 19:23:14 +0300

    save

--- test.php
+++ test.php
@@ -5,7 +5,11 @@
  * Date: 12.09.2016
  * Time: 12:00
  */
+error_reporting(E_ALL);
 require_once __DIR__ . '/vendor/indrig/friday/Friday.php';
 
-$application = new \Friday\Web\Application([]);
+$application = new \Friday\Web\Application([
+    'id' => 'Test Application',
+    'basePath' => __DIR__
+]);
 $application->run();
