6cf82b806b9943d00da5928e3ccf1c4eb5d596fc
igor.bubnevich <igor.bubnevich@zed.com>
2016-09-22 19:38:27 +0300

    save

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,13 +1,25 @@
 <?php
 namespace Application\Controller;
 
+use Friday;
 use Friday\Base\Awaitable;
-use Friday\Promise\Deferred;
 use Friday\Web\Controller;
 
 class IndexController extends Controller{
     public function actionIndex(){
+        $d = new Friday\Base\Deferred();
+        $cache = Friday::$app->getCache();
+        $cache->multiSet([
+            'a' => 1,
+            'b' => 2,
+            'c' => 3
+        ])->await(function ($result) use ($d, $cache) {
+            $cache->get('b')->await(function ($result) use ($d) {
+                var_dump($result);
+                $d->result($this->render('index'));
+            });
 
-        return $this->render('index');
+        });
+        return $d->awaitable();
     }
 }
